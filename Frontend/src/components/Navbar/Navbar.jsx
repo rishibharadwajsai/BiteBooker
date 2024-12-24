de5e919396f226d08d054e125ca15e983124d7b1
Rishi Bharadwaj Sai Lella <rishi.lella123@gmail.com>
2024-12-24 12:57:12 +0530

    Added Cart page

--- Frontend/src/components/Navbar/Navbar.jsx
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -1,30 +1,34 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import "./Navbar.css";
-import {Link} from 'react-router-dom'
+import { Link, Links } from "react-router-dom";
+import { assets } from "../../assets/assets";
+import { StoreContext } from "../../context/StoreContext";
 
-const Navbar = ({setShowLogin}) => {
+const Navbar = ({ setShowLogin }) => {
   const [menu, setMenu] = useState("home");
-
+  const { getTotalCartAmount } = useContext(StoreContext);
   return (
     <div className="navbar">
-      <h1 className="logo">QuickBite</h1>
+      <Link to="/">
+        <h1 className="logo">QuickBite</h1>
+      </Link>
       <ul className="navbar-menu">
         <Link
-        to='/'
+          to="/"
           onClick={() => setMenu("home")}
           className={menu === "home" ? "active" : ""}
         >
           Home
         </Link>
         <a
-        href="#explore-menu"
+          href="#explore-menu"
           onClick={() => setMenu("menu")}
           className={menu === "menu" ? "active" : ""}
         >
           Menu
         </a>
         <a
-        href="#footer"
+          href="#footer"
           onClick={() => setMenu("contact-us")}
           className={menu === "contact-us" ? "active" : ""}
         >
@@ -33,15 +37,10 @@ const Navbar = ({setShowLogin}) => {
       </ul>
       <div className="navbar-right">
         <div className="navbar-basket-icon">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            width="24"
-            height="24"
-          >
-            <path d="M3 6h18l1 12H2L3 6zm2 2l1.5 8h12L19 8H5zM7 18c0 1.104.896 2 2 2s2-.896 2-2-.896-2-2-2-2 .896-2 2zm8 0c0 1.104.896 2 2 2s2-.896 2-2-.896-2-2-2-2 .896-2 2z" />
-          </svg>
-          <div className="dot"></div>
+          <Link to="/cart">
+            <img src={assets.basket_icon} alt="basket_icon" />
+          </Link>
+          <div className={getTotalCartAmount() === 0 ? "" : "dot"}></div>
         </div>
         <button onClick={() => setShowLogin(true)}>Sign in</button>
       </div>
