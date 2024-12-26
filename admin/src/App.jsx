a0563b0d25ee164998269be34f66c5cd8fd458fc
Rishi Bharadwaj Sai Lella <rishi.lella123@gmail.com>
2024-12-26 19:19:27 +0530

    Added functionality to admin panel

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -4,18 +4,22 @@ import Sidebar from "./components/Sidebar/Sidebar";
 import { Route, Routes } from "react-router-dom";
 import List from "./pages/List/List";
 import Orders from "./pages/Orders/Orders";
-import AddItem  from "./pages/AddItem/AddItem";
+import AddItem from "./pages/AddItem/AddItem";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
+  const url = "http://localhost:3000";
   return (
     <div>
+      <ToastContainer />
       <Navbar />
       <hr />
       <div className="app-content">
         <Sidebar />
         <Routes>
-          <Route path="/add" element={<AddItem />} />
-          <Route path="/list" element={<List />} />
+          <Route path="/add" element={<AddItem url={url} />} />
+          <Route path="/list" element={<List url={url} />} />
           <Route path="/orders" element={<Orders />} />
         </Routes>
       </div>
