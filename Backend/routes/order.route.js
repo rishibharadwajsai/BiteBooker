4e5c08f993fd2412762a1bee21e7d13fc0e9155e
Rishi Bharadwaj Sai Lella <rishi.lella123@gmail.com>
2025-01-01 15:43:31 +0530

    Updated the admin panel

--- Backend/routes/order.route.js
+++ Backend/routes/order.route.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import authMiddleware from "../middlewares/auth.js"
-import { placeOrder, userOrders, verifyOrder } from '../controllers/order.controller.js'
+import { listOrders, placeOrder, updateStatus, userOrders, verifyOrder } from '../controllers/order.controller.js'
 
 const orderRouter = express.Router();
 
-orderRouter.post("/place", authMiddleware,placeOrder);
+orderRouter.post("/place", authMiddleware, placeOrder);
 orderRouter.post('/verify', verifyOrder);
 orderRouter.post('/userorders', authMiddleware, userOrders);
+orderRouter.get('/list', listOrders);
+orderRouter.post('/status', updateStatus);
+
 export default orderRouter;
