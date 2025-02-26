b69b6992294cd3ead6b87b2b6ef001010777a2a3
Rishi Bharadwaj Sai Lella <rishi.lella123@gmail.com>
2025-02-26 19:34:51 +0530

    add loading spinner to FoodDisplay component

--- Frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ Frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,29 +1,44 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./FoodDisplay.css";
 import { StoreContext } from "../../context/StoreContext";
 import FoodItem from "../FoodItem/FoodItem";
+
 const FoodDisplay = ({ category }) => {
   const { food_list } = useContext(StoreContext);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (food_list && food_list.length > 0) {
+      setLoading(false);
+    }
+  }, [food_list]);
 
   return (
     <div className="food-display" id="food-display">
       <h2>Top dishes near you</h2>
-      <div className="food-display-list">
-        {food_list.map((item, index) => {
-          if (category === "All" || category === item.category) {
-            return (
-              <FoodItem
-                key={index}
-                _id={item._id}
-                name={item.name}
-                description={item.description}
-                price={item.price}
-                image={item.image}
-              />
-            );
-          }
-        })}
-      </div>
+
+      {loading ? (
+        <div className="loading-spinner">
+          <div className="spinner"></div>
+        </div>
+      ) : (
+        <div className="food-display-list">
+          {food_list.map((item, index) => {
+            if (category === "All" || category === item.category) {
+              return (
+                <FoodItem
+                  key={index}
+                  _id={item._id}
+                  name={item.name}
+                  description={item.description}
+                  price={item.price}
+                  image={item.image}
+                />
+              );
+            }
+          })}
+        </div>
+      )}
     </div>
   );
 };
